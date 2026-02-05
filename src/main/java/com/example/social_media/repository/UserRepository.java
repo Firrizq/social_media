13a5442ed6c48759aebabb743f364ef4966680d6
Firrizq <firrizq@gmail.com>
2026-02-05 09:52:41 +0700

    Add folder repository, add UserRepository

---
+++ src/main/java/com/example/social_media/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.social_media.repository;
+
+import com.example.social_media.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Ini ajaib! Cuma extends JpaRepository, kita langsung punya fitur findByEmail, save, dll.
+public interface UserRepository extends JpaRepository<User, Long> {
+    // Kita tambahkan satu custom method buat cari user via email
+    User findByEmail(String email);
+}
